c4a9612733642d18f6d83700332f71eefb40bae5
Jen <evunti@gmail.com>
2025-04-16 13:46:30 -0400

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,32 @@
 import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <nav className="p-2 flex justify-end">
+          <a className="no-underline font-bold p-2" href="#hero">
+            Home
+          </a>
+          <a className="no-underline font-bold p-2" href="#books">
+            Books
+          </a>
+          <a
+            className="no-underline font-bold p-2"
+            href="https://docs.google.com/spreadsheets/d/1kHmzodxrGFzg5kxeySfrx7rixHIJsCOxClitApXHifI/edit?gid=0#gid=0"
+          >
+            Book Recs
+          </a>
+          <a className="no-underline font-bold p-2" href="#contact">
+            Contact
+          </a>
+        </nav>
+        {children}
+      </body>
+    </html>
+  );
+}
